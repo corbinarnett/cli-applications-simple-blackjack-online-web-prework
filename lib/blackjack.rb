dcd052accf2af31f0246768cddef0e825a3996e8
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-26 01:46:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,12 +3,8 @@ def welcome
 end
 
 def deal_card
-  20.times do
-    card = deal_card
-    expect(card).to be > 0
-    expect(card).to be < 12
-  end
-  end
+
+end
 
 def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
